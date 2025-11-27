a25a2464b6565a54a0eecf21d03da5a12bf9d308
Kevin ADOSSOU <kevin.adossou@epitech.eu>
2025-11-27 16:09:15 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import { useAuthStore } from '@/stores/auth-store';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mvvm-twp.kesug.com/wp-json/wp/v2/',
+  baseURL: 'https://trellolike.free.nf/wp-json/wp/v2/',
   headers: {
     'Content-Type': 'application/json',
     // Authorization: 'Basic ' + btoa('admin:FbLp 0G9G wDG5 BdZi r027 pgHG'),
